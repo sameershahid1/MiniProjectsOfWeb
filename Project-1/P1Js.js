447957fdba524f4ecc162e6d21c633b74c625e24
Sameer Shahid <goku.hulk234@gmail.com>
2022-05-21 22:42:00 +0500

    Mini Project1 Finished

--- Project-1/P1Js.js
+++ Project-1/P1Js.js
@@ -1,1 +1,28 @@
+let LoadText=document.querySelector('.loading-Text');
+let BG=document.querySelector('.bg');
+let Load=1;
+
+let init=setInterval(BlurController,50);
+
+
+function BlurController()
+{
+    if(Load>99)
+    {
+        clearInterval(init);
+    }
+
+    LoadText.innerText=`${Load}%`;
+    LoadText.style.opacity=scale(Load,0,100,1,0);
+    BG.style.filter=`blur(${scale(Load,0,100,30,0)}px)`;
+    Load++;
+}
+
+
+const scale=(num,in_min,in_max,Out_min,Out_max)=>{return ((num-in_min)*(Out_max-Out_min))/(in_max-in_min)+Out_min;};
+
+
+
+
+
 
