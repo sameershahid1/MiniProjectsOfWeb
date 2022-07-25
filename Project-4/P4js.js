076b605f722022ed9f2af4bfc18fda459a5f5f7c
Sameer Shahid <goku.hulk234@gmail.com>
2022-07-25 23:09:36 +0500

    Phase 2 completed

--- Project-4/P4js.js
+++ Project-4/P4js.js
@@ -1,1 +1,14 @@
-const colors=["green","red","rgba(133,122,200)","#f15025"]
+const colors=["green","red","rgba(133,122,200)","#f15025"];
+const btn=document.getElementById('btn');
+const color=document.querySelector(".color");
+const RandomNumberGenerator=()=>(Math.floor(Math.random()*colors.length));
+
+const ColorChanger=()=>{
+    const randomNumber=RandomNumberGenerator(); 
+    document.body.style.backgroundColor=colors[randomNumber];
+    color.textContent=colors[randomNumber];
+};
+
+btn.addEventListener("click",ColorChanger);
+
+
